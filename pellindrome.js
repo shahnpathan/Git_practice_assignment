8e8f6996dfa0665e9396886a2d68995dfba41d80
Tanmoy0151 <royanupama782@gmail.com>
2022-10-20 18:27:00 +0530

    lo tum hara kam ho gaya

--- pellindrome.js
+++ pellindrome.js
@@ -1,5 +1,10 @@
-let sum=0;
-lfuiegfoerfdfe
-fyfdwsncehfeu
-qwertyuiopasdf
-ghjk
+let num=2112;
+let recov=num;
+let bag=0;
+while(num>=1){
+  let  r=num%10;
+    bag=(bag*10)+r;
+    num=num/10
+}
+if(recove==sum){console.log("This is pelindrome");}
+else{"This not a pelindrome"}
