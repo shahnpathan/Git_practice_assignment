420389642fd1d1c2050f4eecc3f2799b6b37d1f5
shahin pathan <shahinpathan9799@gmail.com>
2022-11-02 22:52:39 +0530

    I created prime and palindromice problems file

---
+++ palindrome.js
@@ -0,0 +1,7 @@
+let str="subham";
+let rev="";
+if (str[i]==rev){
+    console.log(yes);
+}else{
+    console.log(no)
+}
---
+++ prime.js
@@ -0,0 +1,5 @@
+let num=5;
+let factor=0;
+if(num%2==0){
+    factor++;
+    }
